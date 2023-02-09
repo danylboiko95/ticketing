c643a4d19f7517f94ec9112124c36ad2c7089314
Danyl Boiko <danyl.boiko_contractor@recordedfuture.com>
2023-02-09 12:40:56 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 //some change
-export * from './errors/bad-request-error'
-export * from './errors/custom-error'
-export * from './errors/database-validation-error'
-export * from './errors/not-authoraized-error'
-export * from './errors/not-found-error'
-export * from './errors/request-validation-error'
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-validation-error";
+export * from "./errors/not-authoraized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-export * from './middlewares/current-user'
-export * from './middlewares/error-handler'
-export * from './middlewares/require-auth'
-export * from './middlewares/validate-request'
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
 
+export * from "./events/base-listener";
+export * from "./events/base-publisher";
+export * from "./events/subjects";
+export * from "./events/ticket-created-event";
+export * from "./events/ticket-updated-event";
